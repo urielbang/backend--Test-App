0371d2a9e7f217ac5c109d2474d56f8e793038d8
urielbang <uri32655@gmail.com>
2024-08-28 11:05:56 +0300

    done backend

--- controllers/pictures.controller.js
+++ controllers/pictures.controller.js
@@ -1,9 +1,12 @@
 const { default: axios } = require("axios");
 
 async function getRandomPicturesLoad(req, res) {
+  const randomPageNumber = Math.round(Math.random() * 26);
+  console.log(randomPageNumber);
+
   try {
     const response = await axios.get(
-      `${process.env.URL}${process.env.API_KEY}`
+      `${process.env.URL}${process.env.API_KEY}&page=${randomPageNumber}`
     );
     const data = await response.data;
     res.status(200).json(data);
@@ -16,4 +19,62 @@ async function getRandomPicturesLoad(req, res) {
   }
 }
 
-module.exports = { getRandomPicturesLoad };
+async function getPictureById(req, res) {
+  const { id } = req.params;
+  try {
+    const response = await axios.get(
+      `${process.env.URL}${process.env.API_KEY}&id=${id}`
+    );
+    const data = await response.data;
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json({
+      data: {
+        status: "fail",
+      },
+    });
+  }
+}
+
+async function getPicturesByPageNumber(req, res) {
+  const { page } = req.params;
+  try {
+    const response = await axios.get(
+      `${process.env.URL}${process.env.API_KEY}&page=${page}`
+    );
+    const data = await response.data;
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json({
+      data: {
+        status: "fail",
+      },
+    });
+  }
+}
+
+async function getPicturesByCategory(req, res) {
+  const { category } = req.params;
+  console.log(category);
+
+  try {
+    const response = await axios.get(
+      `${process.env.URL}${process.env.API_KEY}&category=${category}`
+    );
+    const data = await response.data;
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json({
+      data: {
+        status: "fail",
+      },
+    });
+  }
+}
+
+module.exports = {
+  getRandomPicturesLoad,
+  getPictureById,
+  getPicturesByPageNumber,
+  getPicturesByCategory,
+};
--- routes/picture.routes.js
+++ routes/picture.routes.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { getRandomPicturesLoad } = require("../controllers/pictures.controller");
+const {
+  getRandomPicturesLoad,
+  getPictureById,
+  getPicturesByPageNumber,
+  getPicturesByCategory,
+} = require("../controllers/pictures.controller");
 
 const router = express.Router();
 
 router.route("/").get(getRandomPicturesLoad);
+router.route("/:id").get(getPictureById);
+router.route("/pageNumber/:page").get(getPicturesByPageNumber);
+router.route("/pageCategory/:category").get(getPicturesByCategory);
 
 module.exports = router;
