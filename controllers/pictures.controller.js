9285a1cbb8075042d9d837b535e03d9808f15241
urielbang <uri32655@gmail.com>
2024-09-12 21:32:59 +0300

    update url

--- controllers/pictures.controller.js
+++ controllers/pictures.controller.js
@@ -1,16 +1,18 @@
 const { default: axios } = require("axios");
 
+const url = "https://pixabay.com/api/?key=42079125-1909400cd5615db78c9c2cb93";
+
 async function getRandomPicturesLoad(req, res) {
   const randomPageNumber = Math.round(Math.random() * 26);
   console.log(randomPageNumber);
 
   try {
-    const response = await axios.get(
-      `${process.env.URL}${process.env.API_KEY}&page=${randomPageNumber}`
-    );
+    const response = await axios.get(`${url}&page=${randomPageNumber}`);
     const data = await response.data;
     res.status(200).json(data);
   } catch (error) {
+    console.log(error);
+
     res.status(404).json({
       data: {
         status: "fail",
@@ -22,9 +24,7 @@ async function getRandomPicturesLoad(req, res) {
 async function getPictureById(req, res) {
   const { id } = req.params;
   try {
-    const response = await axios.get(
-      `${process.env.URL}${process.env.API_KEY}&id=${id}`
-    );
+    const response = await axios.get(`${url}&id=${id}`);
     const data = await response.data;
     res.status(200).json(data);
   } catch (error) {
@@ -39,9 +39,7 @@ async function getPictureById(req, res) {
 async function getPicturesByPageNumber(req, res) {
   const { page } = req.params;
   try {
-    const response = await axios.get(
-      `${process.env.URL}${process.env.API_KEY}&page=${page}`
-    );
+    const response = await axios.get(`${url}&page=${page}`);
     const data = await response.data;
     res.status(200).json(data);
   } catch (error) {
@@ -58,9 +56,7 @@ async function getPicturesByCategory(req, res) {
   console.log(category);
 
   try {
-    const response = await axios.get(
-      `${process.env.URL}${process.env.API_KEY}&category=${category}`
-    );
+    const response = await axios.get(`${url}&category=${category}`);
     const data = await response.data;
     res.status(200).json(data);
   } catch (error) {
